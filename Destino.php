d7d8a39f346a997456d526abdc8a436c292d7fb7
rdalvarez <david_de_odin@hotmail.com>
2016-09-05 20:00:41 -0300

    saque comentarios

--- Destino.php
+++ Destino.php
@@ -1,16 +1,5 @@
 
 <?php
-	//var_dump($_GET);//variable global
-	//echo $_GET['nombre'];
-
-	/*condicion 
-	if (isset($_GET['nombre'])) {
-		echo $_GET['nombre'];
-	} 
-	else{
-		echo "Primero ingrese";
-	}*/
-
 	if (isset($_POST['nombre'])) {
 		echo $_POST['nombre'];
 	} 
