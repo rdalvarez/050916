9ea9b62bda9f88a7543d0228e7456cdb0593124e
rdalvarez <david_de_odin@hotmail.com>
2016-09-05 19:46:18 -0300

    condiciones

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body> 
+	<form>
+		<label> Nombre </label>
+		<input type="text" name="nombre" placeholer="Ingrese su nombre">
+
+		<input type="text" name="numeroUno"> 
+		<input type="submit">
+	</form>
+</body>
+</html>
+
+<?php
+	//var_dump($_GET);//variable global
+	//echo $_GET['nombre'];
+
+	//condicion 
+	if (isset($_GET['nombre'])) {
+		echo $_GET['nombre'];
+	} else
+	{
+		echo "Primero ingrese";
+	}
+
+?>
